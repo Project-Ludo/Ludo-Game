c7552e222f5bc66ca0a01ca9e65031cc70f44c9b
wojtek123945 <wojtek34566@gmail.com>
2023-05-16 10:29:16 +0200

    Refractor Factory + creating func to create Platform

--- src/main/java/Factory/LudoFactory.java
+++
@@ -1,6 +0,0 @@
-package Factory;
-
-import com.almasb.fxgl.entity.EntityFactory;
-
-public class LudoFactory implements EntityFactory {
-}
---
+++ src/main/java/LudoFactory.java
@@ -0,0 +1,19 @@
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+public class LudoFactory implements EntityFactory {
+
+    @Spawns("P")
+    public Entity spawnPlatform(SpawnData data){
+        return entityBuilder()
+                .type(EntityType.PLATFORM)
+                .view(new Rectangle(0,0,BLOCK_SIZE, BLOCK_SIZE))
+                .build();
+    }
+
+}
