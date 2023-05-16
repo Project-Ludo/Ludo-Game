185c2fd264cd7196e3c8b68e473dae56c9dbf466
wojtek123945 <wojtek34566@gmail.com>
2023-05-16 11:27:55 +0200

    Add backgroud to game

--- src/main/java/LudoGameApp.java
+++ src/main/java/LudoGameApp.java
@@ -1,13 +1,46 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.core.collection.grid.Cell;
+import com.almasb.fxgl.core.collection.grid.Grid;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.pathfinding.CellState;
+import com.almasb.fxgl.pathfinding.astar.AStarCell;
+import com.almasb.fxgl.pathfinding.astar.AStarGrid;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import javax.swing.text.html.parser.Entity;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+import static com.sun.marlin.MarlinConst.BLOCK_SIZE;
 
 public class LudoGameApp extends GameApplication {
+    private AStarGrid grid;
 
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Bajkowe Ludo");
+    }
+
+    @Override
+    protected void initGame() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+        var clouds = FXGL.getAssetLoader().loadTexture("Clouds V2.png");
+
+        clouds.setFitHeight(600);
+        clouds.setFitWidth(800);
+
+        getGameScene().setBackgroundColor(Color.LIGHTBLUE);
+        getGameScene().addUINode(
+                clouds
+        );
     }
 
     public static void main(String[] args) {
