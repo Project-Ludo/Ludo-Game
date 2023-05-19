13f09699417d3bea2c4ad9fc269991fc66461d0a
wojtek123945 <wojtek34566@gmail.com>
2023-05-19 12:37:18 +0200

    add board to game + testing grid with board

--- src/main/java/LudoGameApp.java
+++ src/main/java/LudoGameApp.java
@@ -10,6 +10,7 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.component.ComponentListener;
 import com.almasb.fxgl.entity.level.Level;
 import com.almasb.fxgl.entity.level.text.TextLevelLoader;
 import com.almasb.fxgl.input.UserAction;
@@ -18,9 +19,19 @@ import com.almasb.fxgl.pathfinding.astar.AStarCell;
 import com.almasb.fxgl.pathfinding.astar.AStarGrid;
 import com.almasb.fxgl.texture.AnimatedTexture;
 import com.almasb.fxgl.texture.Texture;
+import com.sun.javafx.tk.Toolkit;
+import com.sun.source.util.TaskEvent;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
@@ -94,7 +105,7 @@ public class LudoGameApp extends GameApplication {
         for (Entity player:
              bluePlayers) {
             AnimationComponent animationComponent = player.getComponent(AnimationComponent.class);
-            animationComponent.setAnimatedTexture();
+            animationComponent.setAnimatedTextureRun();
             getGameWorld().addEntity(player);
         }
     }
@@ -102,6 +113,18 @@ public class LudoGameApp extends GameApplication {
     @Override
     protected void initUI() {
         setBackground();
+
+        var board = FXGL.getAssetLoader().loadTexture("/background/plansza_Opocity-30%.png");
+        //var board = FXGL.getAssetLoader().loadTexture("/background/plansza.png");
+
+        board.setFitWidth(Config.BOARD_WIDTH);
+        board.setFitWidth(Config.BOARD_HEIGHT);
+
+        board.setLayoutX(Config.BOARD_START_X);
+        board.setLayoutY(Config.BOARD_START_Y);
+
+        GameView gameView = new GameView(board, -5);
+        getGameScene().addGameView(gameView);
     }
 
     private void setBackground(){
@@ -111,7 +134,7 @@ public class LudoGameApp extends GameApplication {
         clouds.setFitWidth(800);
 
         getGameScene().setBackgroundColor(Color.LIGHTBLUE);
-        GameView gameView = new GameView(clouds, -1);
+        GameView gameView = new GameView(clouds, -10);
         getGameScene().addGameView(gameView);
     }
 
