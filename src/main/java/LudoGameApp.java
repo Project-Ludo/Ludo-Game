c0c1ab3094bf74a8d47f126fbbed523c2f0fac41
shorv <kontakt.shorv@gmail.com>
2023-05-20 16:40:51 +0200

    fix entities initializaiton

--- src/main/java/LudoGameApp.java
+++ src/main/java/LudoGameApp.java
@@ -1,16 +1,10 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.app.scene.GameView;
-import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.level.Level;
 import com.almasb.fxgl.entity.level.text.TextLevelLoader;
 import com.almasb.fxgl.pathfinding.CellState;
 import com.almasb.fxgl.pathfinding.astar.AStarGrid;
 import javafx.geometry.Point2D;
-import javafx.scene.paint.Color;
-
-import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getAssetLoader;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
@@ -31,18 +25,17 @@ public class LudoGameApp extends GameApplication {
     protected void initGame() {
         getGameWorld().addEntityFactory(new LudoFactory());
         Level level = getAssetLoader().loadLevel("Ludo.txt", new TextLevelLoader(Config.BLOCK_SIZE, Config.BLOCK_SIZE, '0'));
+        level.getEntities().forEach(entity -> {
+            Point2D fixedPoint = new Point2D(((double) Config.MAP_WIDTH / 2) - ((double) Config.BLOCK_SIZE * Config.MAP_SIZE / 2),
+                    ((double) Config.MAP_HEIGHT / 2) - ((double) Config.BLOCK_SIZE * Config.MAP_SIZE / 2));
+            entity.translate(fixedPoint);
+        });
+
         getGameWorld().setLevel(level);
 
-        //TODO To fixed looking like shit
-        for (int i = 0; i < getGameWorld().getEntities().size(); i++) {
-            Point2D point2D = getGameWorld().getEntities().get(i).getAnchoredPosition();
-            getGameWorld().getEntities().get(i).setPosition(point2D.getX() + 160, point2D.getY() + 60);
-        }
-
         this.grid = AStarGrid.fromWorld(getGameWorld(), Config.MAP_SIZE, Config.MAP_SIZE, Config.BLOCK_SIZE, Config.BLOCK_SIZE, type -> CellState.NOT_WALKABLE);
-
-        List<Entity> entityList = getGameWorld().getEntitiesByType(EntityType.BACKGROUND);
-        entityList.forEach(e -> e.setVisible(false));
+        getGameWorld().getEntitiesByType(EntityType.BACKGROUND)
+                .forEach(entity -> entity.setVisible(false));
     }
 
     @Override
