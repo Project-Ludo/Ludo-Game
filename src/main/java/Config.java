aca2e5af97cfcb945730d4b88a70ef9ae46484f6
Jakub Rychlik <kontakt.shorv@gmail.com>
2023-05-20 17:48:08 +0200

    Create map (#13)
    * Creating map in file
    
    * Update map + create EntityType class
    
    * Creating LudoFactory class
    
    * Refractor Factory + creating func to create Platform
    
    * Creating config file
    
    * Create entity spawn func
    
    * Add backgroud to game
    
    * Refractor backgroud image to separate func
    
    * Creating bg, grid, entity factory
    
    * Adding textures
    
    * Refractor bg image
    
    * remove unused imports
    
    * make config class an utility class
    
    * refactor code
    
    * remove wildcard imports
    
    * initialize entity factory first
    
    * simplify background initialization
    
    * fix entities initializaiton
    
    ---------
    
    Co-authored-by: wojtek123945 <wojtek34566@gmail.com>

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+public final class Config {
+
+    private Config() {
+
+    }
+
+    public static final int BLOCK_SIZE = 30;
+    public static final int MAP_SIZE = 15;
+    public static final int MAP_WIDTH = 1100;
+    public static final int MAP_HEIGHT = 700;
+}
--- src/main/java/LudoGameApp.java
+++ src/main/java/LudoGameApp.java
@@ -1,13 +1,50 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.level.Level;
+import com.almasb.fxgl.entity.level.text.TextLevelLoader;
+import com.almasb.fxgl.pathfinding.CellState;
+import com.almasb.fxgl.pathfinding.astar.AStarGrid;
+import javafx.geometry.Point2D;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getAssetLoader;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 
 public class LudoGameApp extends GameApplication {
 
+    private AStarGrid grid;
+
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(800);
-        settings.setHeight(600);
+        settings.setWidth(Config.MAP_WIDTH);
+        settings.setHeight(Config.MAP_HEIGHT);
         settings.setTitle("Bajkowe Ludo");
+    }
+
+    @Override
+    protected void initGame() {
+        getGameWorld().addEntityFactory(new LudoFactory());
+        Level level = getAssetLoader().loadLevel("Ludo.txt", new TextLevelLoader(Config.BLOCK_SIZE, Config.BLOCK_SIZE, '0'));
+        level.getEntities().forEach(entity -> {
+            Point2D fixedPoint = new Point2D(((double) Config.MAP_WIDTH / 2) - ((double) Config.BLOCK_SIZE * Config.MAP_SIZE / 2),
+                    ((double) Config.MAP_HEIGHT / 2) - ((double) Config.BLOCK_SIZE * Config.MAP_SIZE / 2));
+            entity.translate(fixedPoint);
+        });
+
+        getGameWorld().setLevel(level);
+
+        this.grid = AStarGrid.fromWorld(getGameWorld(), Config.MAP_SIZE, Config.MAP_SIZE, Config.BLOCK_SIZE, Config.BLOCK_SIZE, type -> CellState.NOT_WALKABLE);
+        getGameWorld().getEntitiesByType(EntityType.BACKGROUND)
+                .forEach(entity -> entity.setVisible(false));
+    }
+
+    @Override
+    protected void initUI() {
+        setBackground();
+    }
+
+    private void setBackground() {
+        getGameScene().setBackgroundRepeat("background/background.png");
     }
 
     public static void main(String[] args) {
