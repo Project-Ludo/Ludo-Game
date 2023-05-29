519792e20bd26156616b49d28db75a6c12f87012
shorv <kontakt.shorv@gmail.com>
2023-05-29 20:28:25 +0200

    remove unused constructors and methods

--- src/main/java/controller/SceneController.java
+++ src/main/java/controller/SceneController.java
@@ -13,29 +13,6 @@ public class SceneController {
     private Parent lobby;
     //private final Parent game;
 
-    public SceneController(Parent menuStart, Parent menuRules, Parent menuConnection, Parent lobby, Parent game) {
-        //this.game = game;
-        this.menuStart = menuStart;
-        this.menuConnection = menuConnection;
-        this.menuRules = menuRules;
-        this.lobby = menuRules;
-    }
-
-    public SceneController(Parent menuStart, Parent menuRules, Parent menuConnection) {
-        this.menuStart = menuStart;
-        this.menuRules = menuRules;
-        this.menuConnection = menuConnection;
-
-        menuStart.setLayoutX(Config.MENU_START_LAYOUT_X);
-        menuStart.setLayoutY(Config.MENU_START_LAYOUT_Y);
-
-        menuRules.setLayoutX(Config.MENU_START_LAYOUT_X);
-        menuRules.setLayoutY(Config.MENU_START_LAYOUT_Y);
-
-        menuConnection.setLayoutX(Config.MENU_START_LAYOUT_X);
-        menuConnection.setLayoutY(Config.MENU_START_LAYOUT_Y);
-    }
-
     public SceneController(Parent menuStart, Parent menuRules, Parent menuConnection, Parent lobby) {
         this.menuStart = menuStart;
         this.menuRules = menuRules;
@@ -59,32 +36,16 @@ public class SceneController {
         return menuStart;
     }
 
-    public void setMenuStart(Parent menuStart) {
-        this.menuStart = menuStart;
-    }
-
     public Parent getMenuRules() {
         return menuRules;
-    }
-
-    public void setMenuRules(Parent menuRules) {
-        this.menuRules = menuRules;
     }
 
     public Parent getMenuConnection() {
         return menuConnection;
     }
 
-    public void setMenuConnection(Parent menuConnection) {
-        this.menuConnection = menuConnection;
-    }
-
     public Parent getLobby() {
         return lobby;
-    }
-
-    public void setLobby(Parent lobby) {
-        this.lobby = lobby;
     }
 
     public void changeSceneAfter(Parent nextScene, double millis){
