e0b1a326cb194783b5d48f453a0aa52d51677f66
wojtek123945 <wojtek34566@gmail.com>
2023-06-01 11:26:00 +0200

    add aniamtion component to player factory

--- src/main/java/factory/LudoFactory.java
+++ src/main/java/factory/LudoFactory.java
@@ -1,5 +1,8 @@
 package factory;
 
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import component.AnimationComponent;
 import config.Config;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
@@ -50,7 +53,12 @@ public class LudoFactory implements EntityFactory {
     public Entity spawnPlayer(SpawnData data, PlayerType playerType) {
         return entityBuilder(data)
                 .type(EntityType.PLAYER)
-                .viewWithBBox(new Rectangle(Config.BLOCK_SIZE, Config.BLOCK_SIZE, Color.BLACK))
+                .with(new AnimationComponent(playerType))
+                .onClick(entity -> {
+                    AnimationComponent animationComponent = entity.getComponent(AnimationComponent.class);
+                    animationComponent.switchAnimation();
+                })
+                .bbox(new HitBox(BoundingShape.box(32, 32)))
                 .build();
     }
 }
